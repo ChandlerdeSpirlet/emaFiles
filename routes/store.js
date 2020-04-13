57b8fb7c88a867f0d4cc08542789bceacd867510
Chandler de Spirlet <chandler.despirlet@icloud.com>
2020-04-12 18:41:01 -0600

    Update store.js

--- routes/store.js
+++ routes/store.js
@@ -278,17 +278,20 @@ app.post('/preview/(:fname)/(:lname)/(:jj)/(:pu)/(:su)/(:mtn_cl)/(:fk)', functio
                 console.log('in .then');
             })
             .catch(function(err){
+                can_redir = false;
                 console.log("In .catch");
                 console.log('error is ' + err);
                 req.flash('error', 'Unable to add progress check data (ERROR: ' + err + ')');
                 res.redirect('student_progress_check')
             })
+            /*
             console.log('can_redir = ' + can_redir);
             if (can_redir == true){
                 var stud = req.params.fname + " " + req.params.lname;
                 var redir = 'good_job/' + stud;
                 res.redirect(redir);
             }
+            */
     }
     if (item.button == 'Edit'){
         res.render('store/student_progress_check', {
