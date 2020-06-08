443cbe5ce539d7656e5e7b6de214958e631007ee
Chandler de Spirlet <chandler.despirlet@icloud.com>
2020-06-08 01:01:11 -0600

    Working on data

--- routes/store.js
+++ routes/store.js
@@ -1264,14 +1264,15 @@ app.get('/classes_email/(:email)', function(req, res){
     var query = "select id, id_from_other, first_name, last_name, cast(to_char(test_day, 'Mon DD, YYYY') as varchar) as test_day_var, test_time from class_signups where email = $1 and test_day >= now() order by test_day";
     db.query(query, [req.params.email])
     .then(function(rows){
-        if (rows.length = 0){
+        if (rows.length == 0){
             req.flash('error', 'Unable to find classes with the email ' + req.params.email);
             res.redirect('store/email_lookup');
+        } else {
+            res.render('store/classes_email', {
+                email: req.params.email,
+                data: rows
+            })
         }
-        res.render('store/classes_email', {
-            email: req.params.email,
-            data: rows
-        })
     })
     .catch(function(err){
         req.flash('error', 'Cound not render any classes associated with the email ' + item.email + '. ERR_no: ' + err);
@@ -1284,6 +1285,6 @@ app.get('/classes_email/(:email)', function(req, res){
 app.get('/classes_email', function(req, res){
     res.render('store/classes_email', {
         email: '',
-        data: rows
+        data: ''
     })
 });
