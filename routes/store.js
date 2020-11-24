2998b119eaee8281e4ef5c64fc76e863748e4ea3
Chandler de Spirlet <chandler.despirlet@gmail.com>
2020-11-23 20:15:50 -0700

    Fixing count issue

--- routes/store.js
+++ routes/store.js
@@ -2542,7 +2542,7 @@ app.get('/temp_classes', function(req, res){
 });
 
 app.get('/board_breaking', function(req, res){
-    const query = 'select * from board_breaking_times where count < 20 order by class_time';
+    const query = 'select * from board_breaking_times where count < 26 order by class_time';
     db.any(query)
         .then(function(rows){
             res.render('store/board_breaking', {
