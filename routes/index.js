86d48c054b894c9658610d36da27c41579ae4fd4
Chandler de Spirlet <chandler.despirlet@icloud.com>
2022-02-06 09:27:59 -0700

    Testing API

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var app = express();
+var db = require('../database');
 module.exports = app;
 app.get('/', function (req, res){
     res.render('store/home', {
         
     })
 });
+
+app.get('/test', (req, res, next) => {
+    res.json({
+        message: 'alive'
+    });
+})
